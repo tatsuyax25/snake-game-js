2ab6489fb23208a48a0817fb128d25136b1c5e5c
Miguel Urena <urena_miguel82@outlook.com>
2023-12-27 13:31:09 -0800

    Creating snake logic

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 // Define HTML elements
 const board = document.getElementById('game-board');
 
+// Define game variables
+let snake = [
+  {x: 10, y: 10},
+]
+
+// Draw game map, snake, food, etc.
+function draw() {
+  board.innerHTML = '';
+  drawSnake();
+}
+
+// Draw snake
+function drawSnake() {
+  // code to draw the snake
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement('div', 'snake');
+  });
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
